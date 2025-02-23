b06e819906300b87fbe4e1b9d5a2fba91cf78b25
Rushabh <rushabhbarot812@gmail.com>
2025-02-23 21:33:50 +0530

    function for fetching video and channel statistics

--- YoutubeCommentScrapper.py
+++ YoutubeCommentScrapper.py
@@ -27,3 +27,40 @@ def get_channel_id(video_id):
     except HttpError as error:
         print(f'YouTube API Error: {error}')
         return None
+    
+def get_video_stats(video_id):
+    """Fetches video statistics (views, likes, etc.)."""
+    try:
+        response = youtube.videos().list(part='statistics', id=video_id).execute()
+        if not response['items']:
+            print("Error: Invalid Video ID")
+            return None
+        return response['items'][0]['statistics']
+    except HttpError as error:
+        print(f'YouTube API Error: {error}')
+        return None
+
+def get_channel_info(channel_id):
+    """Fetches YouTube channel details."""
+    try:
+        response = youtube.channels().list(
+            part='snippet,statistics,brandingSettings',
+            id=channel_id
+        ).execute()
+
+        if not response['items']:
+            print("Error: Invalid Channel ID")
+            return None
+
+        info = response['items'][0]
+        return {
+            'channel_title': info['snippet']['title'],
+            'video_count': info['statistics']['videoCount'],
+            'channel_logo_url': info['snippet']['thumbnails']['high']['url'],
+            'channel_created_date': info['snippet']['publishedAt'],
+            'subscriber_count': info['statistics']['subscriberCount'],
+            'channel_description': info['snippet']['description']
+        }
+    except HttpError as error:
+        print(f'YouTube API Error: {error}')
+        return None
